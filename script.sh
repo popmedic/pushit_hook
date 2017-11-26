51cdfcb0ccce01eae92d154f0c5ffbfc266482ae
kscardina <kscardina@webroot.com>
2017-11-26 08:35:52 -0700

    run afplay in bg proc

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo "#!/bin/sh\nafplay pushit.mp3\nexit 0\n" > .git/hooks/pre-push
+echo "#!/bin/sh\nafplay pushit.mp3 &\nexit 0\n" > .git/hooks/pre-push
 chmod guo+x .git/hooks/pre-push
 curl -o .git/hooks/pushit.mp3 "https://github.com/popmedic/pushit_hook/blob/master/pushit.mp3?raw=true"
